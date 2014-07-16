897c234fa485b5324ef16e8b6ce73ecf77a18579
Vinodh Rajan <vinodh.vinodh@gmail.com>
2014-07-16 18:55:53 +0100

    Adding AGPL license

--- transliteration.php
+++ transliteration.php
@@ -1,25 +1,21 @@
 ﻿<?PHP
 
 /*
+ * Copyright (C) 2014 Vinodh Rajan vinodh@virtualvinodh.com
+ *
+ * This file is a part of Anunaadam.
+ *
+ * Anunaadam is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version. This program is distributed in the hope that it
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General
+ * Public License for more details. You should have received a copy of the GNU
+ * Affero General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
 
-Copyright (C) 2013 Vinodh Rajan vinodh@virtualvinodh.com
-
-This file is a part of Aaravam.
-
-Avalokitam is free software: you  can redistribute it and/or modify it
-under the  terms of the GNU  General Public License as  published by the
-Free Software Foundation,  either version 3 of the License,  or (at your
-option) any later version.
-
-This  program  is distributed  in  the  hope  that  it will  be  useful,
-but  WITHOUT  ANY  WARRANTY;  without   even  the  implied  warranty  of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
-Public License for more details.
-
-You should have received a copy  of the GNU General Public License along
-with this program. If not, see <http://www.gnu.org/licenses/>.
-
-*/
 
 
 function tam2lat($text)
